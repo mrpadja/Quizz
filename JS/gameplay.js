bd770ba15179ac6221bd7faaa0a81af365d8f44f
Damas Serius <seriusdamas@gmail.com>
2021-07-21 21:18:27 -0300

    Gameplay functions crated

--- JS/gameplay.js
+++ JS/gameplay.js
@@ -1,8 +1,134 @@
+
 var ShowImage = document.querySelector("#Show");
+var lives = document.querySelector(".lives")
+let check = document.querySelector(".yes")
+let avalaible = array_();
+const AnswerContainer = document.querySelector(".answers")
+let key = Math.floor(Math.random() * avalaible.length);
+let over = document.querySelector('GameOver')
+let Playerscore = document.querySelector('.scorenumber');
+let GoodAnswer =  new Audio('./Assets/music/correct.mp3');
+let BadAnswer =  new Audio('./Assets/music/wrong.mp3');
+let GameOverSound =  new Audio('./Assets/music/gameOver2.wav');
+GameOverScore = document.querySelector('.score_End')
 
-console.log(array_())
+lives.innerHTML= 3;  
+Playerscore.innerHTML = 0;
+GameOverScore.innerHTML = 0;
 
-key = Math.floor(Math.random() * array_().length);
-console.log(key)
+user = window.localStorage.getItem('User');
 
-ShowImage.src = array_()[key].imgUrl;
+
+
+
+
+
+
+function next(){
+    let q = key;
+    if(avalaible.length > 0){
+    if (q > -1) {
+        avalaible.splice(q, 1);
+      }
+    }
+    return [avalaible]
+}
+
+
+
+ 
+
+
+
+
+function gameOver(){
+    GameOverSound.play();
+    document.getElementById("Over").style.display = "flex";
+    document.getElementById("player").innerHTML = user;
+    GameOverScore.innerHTML = Playerscore.innerHTML ;
+
+}
+
+timeout =''
+timeout2=''
+
+
+
+
+
+ function start() {
+
+    console.log("TOPPPPPPPPPPPPPPP")
+    
+    if(typeof avalaible.length !== 'undefined' && avalaible.length > 0 && avalaible){
+        ShowImage.src = avalaible[key].imgUrl;
+        for(let i=1; i<=4; i++){
+            document.getElementById(String(i)).innerText= avalaible[key].rep[i-1];
+        }
+        
+    
+        
+       timeout = window.setTimeout(function(){
+            if(Number(lives.innerHTML) > 0){
+                next();  
+                key = Math.floor(Math.random() * avalaible.length);
+                lives.innerHTML--;  
+                start()
+            }
+        },10000);
+        if(Number(lives.innerHTML) === 0){
+            gameOver()
+            window.clearTimeout(timeout)
+        }
+
+        
+
+    }else{
+        gameOver()
+    }
+   };
+
+
+
+
+   AnswerContainer.addEventListener('click', (event) => {
+        const isButton = event.target.nodeName === 'BUTTON';
+        if (!isButton) {
+        return;
+        }
+        if (typeof avalaible[key] !== 'undefined' && event.target.innerText === avalaible[key].rep[avalaible[key].real]){
+            document.getElementById(String((avalaible[key].real+1))).classList.add('correct');
+            // check.style.display = "block"
+                            let score = Number(Playerscore.innerHTML) + 10
+                            GoodAnswer.play()
+                            console.log(score)
+                            Playerscore.innerHTML = score
+                            
+
+                            timeout2 = window.setTimeout(function(){
+                                document.getElementById(String((avalaible[key].real+1))).classList.remove('correct');
+                                    next();
+                                    key = Math.floor(Math.random() * avalaible.length);
+                                    console.log('vraaaaaaaaaaaaaa')
+                                    start();
+
+                            },1000);
+                            
+                            
+        }else{
+            event.target.classList.add('bad');
+            BadAnswer.play();
+            lives.innerHTML--; 
+            timeout2 = window.setTimeout(function(){
+                event.target.classList.remove('bad');
+                    next();
+                    key = Math.floor(Math.random() * avalaible.length);
+                    console.log('vraaaaaaaaaaaaaa')
+                    start();
+
+            },1000);
+        }
+    window.clearTimeout(timeout)
+  })
+
+  window.onload= start()
--- JS/questions.js
+++ JS/questions.js
@@ -4,42 +4,63 @@ function array_(){
     return [
         {
             imgUrl :"https://im-media.voltron.voanews.com/Drupal/01live-166/styles/sourced/s3/reuters-pictures/2019/09/RTS2PDBP.jpg?itok=jMVf4i8B",
-            rep1 : { answer : "Mugabe", Real : true} , 
-            rep2 : { answer : "TAjax", Real : false},
-            rep3 : { answer : "Babika", Real : false},
-            rep4 : { answer : "Jod", Real: false}
+            rep:['Robert Mugabe', 'Nelson Chamisa', 'Jovenel Moïse','Kembo Mohadi'],
+            real: 0
         },
 
         {
+            
             imgUrl :"https://observatoriodocinema.uol.com.br/wp-content/uploads/2021/03/Arnold-Schwarzenegger-1.jpg",
-            rep1 : { answer : "Jet lee", Real : false} , 
-            rep2 : { answer : "TAjax", Real : false},
-            rep3 : { answer : "Rambo", Real : true},
-            rep4 : { answer : "Jean Claude", Real: false}
+            rep:["JC Van Damme", "Sylvester Stallone", "Arnold Schwarzenegger","Steven Seagal" ],
+            real:2
         },
 
         {
             imgUrl :"https://cdn.esbrasil.com.br/wp-content/uploads/2021/06/22112317/chris-brown.jpg",
-            rep1 : { answer : "Justin Bieber", Real : false} , 
-            rep2 : { answer : "Usher", Real : false},
-            rep3 : { answer : "Chris Brown", Real : true},
-            rep4 : { answer : "Izolan", Real: false}
+            rep: ["Usher", "Justin Bieber", "Chris Brown", "Rotimi"],
+            real:2
         },
 
         {
             imgUrl :"https://s2.glbimg.com/1B5TMlcihRI3VcmDF2VeInvbB-U=/smart/e.glbimg.com/og/ed/f/original/2020/06/22/justin-bieber-1.jpg",
-            rep1 : { answer : "Justin Bieber", Real : true} , 
-            rep2 : { answer : "Jeden Smith", Real : false},
-            rep3 : { answer : "Shawn Mendes", Real : false},
-            rep4 : { answer : "Lil B", Real: false}
+            rep: ["Charlie Puth", "Justin Bieber", "Chawn Mendez", "Lil tay"],
+            real:1
         },
 
         {
             imgUrl :"https://static.toiimg.com/thumb/msid-84201735,width-1070,height-580,imgsize-222834,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg",
-            rep1 : { answer : "Jean Bertrand Aristide", Real : false} , 
-            rep2 : { answer : "Ti Claude", Real : false},
-            rep3 : { answer : "Michel Martelly", Real : false},
-            rep4 : { answer : "Jovenel Moise", Real: false}
+            rep: ["Michel Martely", "JB Aristide", "Moise Jn Charles", "Jovenel Moise"],
+            real:3
+        },
+
+        {
+            imgUrl :"https://s2.glbimg.com/-35xwpnOIpGcceHEWl5WLYZPlRY=/0x0:660x371/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2019/I/1/uEM4rYR2iKdLdBgIToaA/pewdiepie.jpg",
+            rep: ["Pewdiepie", "Jackseptic", "Mr Beast", "David Dobrik"],
+            real:0
+        },
+
+        {
+            imgUrl :"https://s2.glbimg.com/hOQeWyRlurqGohHz2rDzpirVqgo=/e.glbimg.com/og/ed/f/original/2021/06/26/gettyimages-1229892421.jpg",
+            rep: ["Jeff Bezos", "Steve Jobs", "Elon Musk", "Bill Gates"],
+            real:2
+        },
+
+        {
+            imgUrl :"https://s2.glbimg.com/s8k4JM4K49yX5LJ4vNQORZhajBU=/940x523/top/e.glbimg.com/og/ed/f/original/2021/03/12/gettyimages-98329977.jpg",
+            rep: ["Steve Wozniak", "Steve Jobs", "Tim Cook", "Bill Gates"],
+            real:1
+        },
+
+        {
+            imgUrl :"https://static.glamurama.uol.com.br/2019/06/Rihanna.jpg",
+            rep: ["Beyoncé", "Jennifer Aniston", "Nicki Minaj", "Rihanna"],
+            real:3
+        },
+
+        {
+            imgUrl :"https://assets.teenvogue.com/photos/60f5a271751e6ffb59ca45dd/16:9/w_2560%2Cc_limit/GettyImages-1233949624.jpg",
+            rep: ["Zendaya", "Bella Thorne", "Tana Mongeau", "Kendall Jenner"],
+            real:0
         },
     ]
 
